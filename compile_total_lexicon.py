074726c4f22312274a52f7a3e025fb5cc07c70e6
drupchen <hhdrupchen@gmail.com>
2017-04-21 14:30:15 +0200

    bugfix : the entries with no affix but with an extra dadrag(ex: ཀུནད་དགའ    ཀུན་དགའ) were not generated.

--- compile_total_lexicon.py
+++ compile_total_lexicon.py
@@ -8,11 +8,14 @@ particles = [tuple(a.split(' ')) for a in particles_raw.strip().split('\n')]
 
 lexicon_raw = open_file('output/lexicon_with_markers.txt')
 lexicon = lexicon_raw.strip().split('\n')
-expansion_affixes = ['འི', 'འོ', 'ས', 'ར']
+A_expansion = ['འི', 'འོ', 'འའིས', 'ས', 'ར']
+B_expansion = ['འི', 'འོ', 'ས', 'ར']
 
 lexicon_expanded = []
 for word in lexicon:
     base_form = word.replace('/A', '').replace('/B', '').replace('/C', '')
+    if base_form == 'ཀུན་དགའ':
+        print('ok')
     # expand all forms with dadrag
     with_dadrag = []
     if '/C' in word:
@@ -27,14 +30,18 @@ for word in lexicon:
         if '/' in form:
             to_affix, kind = form.split('/')
             if kind == 'A':
+                with_affixed.append(to_affix)
                 to_affix = to_affix.rstrip('འ')
-            for affix in expansion_affixes:
-                with_affixed.append(to_affix + affix)
+                for affix in A_expansion:
+                    with_affixed.append(to_affix + affix)
+            if kind == 'B':
+                with_affixed.append(to_affix)
+                for affix in B_expansion:
+                    with_affixed.append(to_affix + affix)
         else:
             with_affixed.append(form)
 
     # fill lexicon_expanded with all the forms found
-    lexicon_expanded.append((base_form, base_form))  # add the one without modifications
     for final in with_affixed:
         lexicon_expanded.append((final, base_form))
 
