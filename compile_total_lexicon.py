af42ad59a8235987cfd15e7818289db1aac6e68f
drupchen <hhdrupchen@gmail.com>
2017-05-08 21:17:26 +0200

    bugfix

--- compile_total_lexicon.py
+++ compile_total_lexicon.py
@@ -11,22 +11,32 @@ lexicon = lexicon_raw.strip().split('\n')
 A_expansion = {'འི': '>A', 'འོ': '>B', 'ས': '>C', 'ར': '>D', 'འའིས': '>E'}
 B_expansion = {'འི': '>a', 'འོ': '>b', 'ས': '>c', 'ར': '>d', 'འིས': '>e'}
 # operations to reconstruct the lemma
-cmds = {'>A': ('-འི', '+འ'),
-        '>a': ('-འི',),
-        '>B': ('-འོ', '+འ'),
-        '>b': ('-འོ',),
-        '>C': ('-ས', '+འ'),
-        '>c': ('-ས',),
-        '>D': ('-ར', '+འ'),
-        '>d': ('-ར',),
-        '>E': ('-འའིས', '+འ'),
-        '>e': ('-འིས',)
+cmds = {'>A': ('-2', '+འ'),
+        '>a': ('-2',),
+        '>B': ('-2', '+འ'),
+        '>b': ('-2',),
+        '>C': ('-1', '+འ'),
+        '>c': ('-1',),
+        '>D': ('-1', '+འ'),
+        '>d': ('-1',),
+        '>E': ('-4', '+འ'),
+        '>e': ('-3',)
         }
+
+
+def apply_cmds(form, cmd, cmds):
+    to_apply = cmds[cmd]
+    for mod in to_apply:
+        if mod.startswith('-'):
+            form = form[:len(form)-int(mod[1])]
+        if mod.startswith('+'):
+            form = form+mod[1:]
+    return form
 
 lexicon_expanded = []
 for word in lexicon:
     base_form = word.replace('/A', '').replace('/B', '').replace('/C', '')
-    if base_form == 'ཀུན་དགའ':
+    if base_form == 'རྒྱབ་རྫི':
         print('ok')
     # expand all forms with dadrag
     with_dadrag = []
@@ -45,11 +55,17 @@ for word in lexicon:
                 with_affixed.append((to_affix, '='))
                 to_affix = to_affix.rstrip('འ')
                 for affix in sorted(A_expansion):
-                    with_affixed.append((to_affix + affix, A_expansion[affix]))
+                    if apply_cmds(to_affix+affix, A_expansion[affix], cmds) == base_form:
+                        with_affixed.append((to_affix + affix, A_expansion[affix]))
+                    else:
+                        with_affixed.append((to_affix + affix, base_form))
             if kind == 'B':
                 with_affixed.append((to_affix, '='))
                 for affix in sorted(B_expansion):
-                    with_affixed.append((to_affix + affix, B_expansion[affix]))
+                    if apply_cmds(to_affix+affix, B_expansion[affix], cmds) == base_form:
+                        with_affixed.append((to_affix + affix, B_expansion[affix]))
+                    else:
+                        with_affixed.append((to_affix + affix, base_form))
         else:
             with_affixed.append((form, '='))
 
