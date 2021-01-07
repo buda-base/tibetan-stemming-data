a0c02651187566813db7e879875622cdcd3153c3
eroux <elie.roux@telecom-bretagne.eu>
2021-01-07 18:18:13 +0100

    add simple verb substitution thingy

---
+++ prepare_verbs_simple.py
@@ -0,0 +1,23 @@
+import json
+
+dadragsyls = []
+with open('input/dadrag_syllables.txt') as f:
+    dadragsyls = f.read().split('\n')
+
+with open('input/monlam_verbs.json') as json_file:
+    data = json.load(json_file)
+
+    for inflected, contexts in data.items():
+        if len(contexts) == 0:
+            continue
+
+        possible = set()
+        for context in contexts:
+            if 'བྱ་ཚིག' in context.keys():
+                possible.add(context['བྱ་ཚིག'])
+
+        possible = list(possible)
+        if len(possible) == 1 and possible[0] != inflected:
+            print(inflected+","+possible[0])
+            if inflected in dadragsyls:
+                print(inflected+"ད,"+possible[0])
